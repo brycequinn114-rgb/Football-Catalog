c94e946b4f6a1583a8a10af51b29960d9df0c57f
brycequinn114-rgb <brycequinn114@gmail.com>
2026-06-02 20:39:04 -0400

    Add files via upload

---
+++ games.js
@@ -0,0 +1,61 @@
+const games = [
+  {
+    title: "Griswold Vs Montville",
+    date: "November, 2000",
+    score: "N/A",
+    result: "loss",
+    note: "N/A",
+    video: "lBGbG9bUxRM"
+  },
+  {
+    title: "Griswold Vs Planifield ",
+    date: "Nov, 1994",
+    score: "20 - 14",
+    result: "win",
+    note: "1st Victory Over Plainfield",
+    video: "axMm39qXfks"
+  },
+  {
+    title: "Game 3",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  },
+  {
+    title: "Game 4",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  },
+  {
+    title: "Game 5",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  },
+  {
+    title: "Game 6",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  },
+  {
+    title: "Game 7",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  },
+  {
+    title: "Game 8",
+    date: "",
+    score: "",
+    note: "",
+    video: ""
+  }
+];
+
