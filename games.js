f484c070c2625f0eb53dfb227bf850effc7af7cb
brycequinn114-rgb <brycequinn114@gmail.com>
2026-06-09 20:26:33 -0400

    Update games.js

--- games.js
+++ games.js
@@ -2,7 +2,7 @@ const games = [
   {
     title: "Griswold Vs Montville",
     date: "Nov, 2000",
-    score: "8 - 20",
+    score: "8-20",
     result: "loss",
     note: "First and Only ECC loss.",
     video: "lBGbG9bUxRM"
@@ -10,7 +10,7 @@ const games = [
   {
     title: "Griswold Vs Plainfield ",
     date: "Nov, 1994",
-    score: "20 - 14",
+    score: "20-14",
     result: "win",
     note: "1st Victory Over Plainfield.",
     video: "axMm39qXfks"
@@ -24,11 +24,12 @@ const games = [
     video: "mm362cyMBZ0"
   },
   {
-    title: "Game 4",
-    date: "",
-    score: "",
+    title: "Griswold Vs Bacon",
+    date: "Nov, 2004",
+    score: "13-12",
+    result:"win",
     note: "",
-    video: ""
+    video: "fzVnM2ltd6g"
   },
   {
     title: "Game 5",
